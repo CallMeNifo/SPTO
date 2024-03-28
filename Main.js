992bd4e880f90bfdf88cf637d6493a2bb57e3e1b
CallMeNifo <164952002+CallMeNifo@users.noreply.github.com>
2024-03-28 04:13:52 +0300

    Add files via upload

--- Main.js
+++ Main.js
@@ -1,78 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     var loaderWrapper = document.querySelector(".loader-wrapper");
 
-    // Hide the loader after a delay (e.g., 2000 milliseconds)
     setTimeout(function() {
         loaderWrapper.style.display = "none";
-    }, 1000); // Adjust the delay time as needed
+    }, 1000);
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-    var scrollDownText = document.getElementById("scroll-text");
-
-    scrollDownText.addEventListener("click", function() {
-        var targetSection = document.querySelector(".body");
-        var targetSectionPosition = targetSection.offsetTop;
-        var currentPosition = window.pageYOffset;
-        var distance = targetSectionPosition - currentPosition;
-
-        var duration = 1000;
-        var start = null;
-
-        function step(timestamp) {
-            if (!start) start = timestamp;
-            var progress = timestamp - start;
-            window.scrollTo(0, easeInOutQuad(progress, currentPosition, distance, duration));
-            if (progress < duration) {
-                window.requestAnimationFrame(step);
-            }
-        }
-
-        window.requestAnimationFrame(step);
-    });
-});
-
-function easeInOutQuad(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return c / 2 * t * t + b;
-    t--;
-    return -c / 2 * (t * (t - 2) - 1) + b;
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    var scrollButton = document.getElementById("bak2top");
-
-    scrollButton.addEventListener("click", function (e) {
-        e.preventDefault();
-
-        var scrollDuration = 300; 
-        var scrollStep = -window.scrollY / (scrollDuration / 15);
-        var scrollInterval = setInterval(function () {
-            if (window.scrollY !== 0) {
-                window.scrollBy(0, scrollStep);
-            } else {
-                clearInterval(scrollInterval);
-            }
-        }, 15);
-    });
-
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 100) {
-            scrollButton.style.display = "block";
-        } else {
-            scrollButton.style.display = "none";
-        }
-    });
-});
-
-function toggleDropdown() {
-    var dropdownContent = document.getElementById("lang-content");
-    if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-    } else {
-        dropdownContent.style.display = "block";
-    }
-}
 
 document.addEventListener("DOMContentLoaded", function() {
     var scrollDownText = document.getElementById("scroll-text");
